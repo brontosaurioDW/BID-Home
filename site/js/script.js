64d065367449326e8ee279984913bcc7f6d6b828
Flor Espino <fespino@perceptyx.com>
2020-06-10 22:52:04 -0400

    Home - Work in progress
    Desktop almost ready
    Mobile is still a mess
    Content is all done

--- site/js/script.js
+++ site/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 		infinite: true,
 		speed: 300,
 		slidesToShow: 1,
-		adaptiveHeight: true,
+		adaptiveHeight: false,
 		nextArrow:"<img class='a-right control-c next slick-next' src='img/icons/right.png'>",
 		prevArrow:"<img class='a-left control-c prev slick-prev' src='img/icons/left.png'>"
 	});
